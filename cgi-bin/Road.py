fadee64696ac441a83aaf5677ec7dcf5be32a6b0
tungh1850 <39918507+tungh1850@users.noreply.github.com>
2018-06-11 15:25:13 +0200

    Update Road Generator

--- cgi-bin/Road.py
+++ cgi-bin/Road.py
@@ -1,29 +1,30 @@
 __author__ = 'tunghoang'
 import numpy as np
-#from  DnD import Dungeon
+from  DnD import Dungeon
+import matplotlib.pyplot as plt
+
 """
-temp = np.zeros((10,10), dtype=np.int)
-temp[1:3, 1:3] = 1
-temp[1:3, 1:3] = 1
+Road Generator Version 1.0001
 """
 
 class Road:
     map = []                # DnD map
     field = []              # enthält alle Knoten, die keine Raum sind
     graph = {}              # Liste die Knoten, die untereinander adjazent sind
+    costList = {}
     road = []               # alle Knoten vom endliche Weg
     widthOfMap = 0
     heightOfMap = 0
 
+
     # Initialisierung vom Weg-Objekt Konstruktur
-    def __init__(self, map, startNode, endNode):
+    def __init__(self, map,):
         self.map = map
-        self.startNode = startNode[::-1]
-        self.endNode = endNode[::-1]
+
         self.widthOfMap = np.shape(self.map)[0]
         self.heightOfMap = np.shape(self.map)[1]
         self.mapScanner()
-        self.pfad_breitensuche()
+        #self.pfad_breitensuche()
 
 
     # diese Methode überprüft alle Knoten in der Karte und dann:
@@ -35,8 +36,8 @@ class Road:
         for row in range(self.heightOfMap):
             for column in range(self.widthOfMap):
 
-                if self.map[row][column] == 0:                           #map[y_pos][x_pos] = 0 bedeutet, dass diese Knote frei ist
-                    self.field.append((row,column))                      #y_pos und x_pos werden verbunden als string mit der Form 'y_pos + x_pos'
+                if self.map[row][column] == 0 or self.map[row][column] == 20:   #map[y_pos][x_pos] = 0 bedeutet, dass diese Knote frei ist
+                    self.field.append((row,column))                             #y_pos und x_pos werden verbunden als string mit der Form 'y_pos + x_pos'
 
 
         # self.graph erzeugen
@@ -48,15 +49,33 @@ class Road:
 
 
             #  Knoten auf self.graph einfügen
-            if (right in self.field):     self.graph.setdefault(key, []).append(right)
-            if (bottom in self.field):    self.graph.setdefault(key, []).append(bottom)
-            if (left in self.field):      self.graph.setdefault(key, []).append(left)
-            if (top in self.field):       self.graph.setdefault(key, []).append(top)
+            if (right in self.field):
+                self.graph.setdefault(key, []).append(right)
+                self.costList.setdefault(right )
+                self.costList[right] = 1
+
+            if (bottom in self.field):
+                self.graph.setdefault(key, []).append(bottom)
+                self.costList.setdefault(bottom)
+                self.costList[bottom] = 1
+
+            if (left in self.field):
+                self.graph.setdefault(key, []).append(left)
+                self.costList.setdefault(left)
+                self.costList[left] = 1
+
+            if (top in self.field):
+                self.graph.setdefault(key, []).append(top)
+                self.costList.setdefault(top)
+                self.costList[top] = 1
+
 
         return self.graph
 
     #Breadth-First-Search implementieren mit startNode und endNode
-    def pfad_breitensuche(self):
+    def pfad_breitensuche(self,startNode,endNode):
+        self.startNode = startNode[::-1]
+        self.endNode = endNode[::-1]
 
         check1 = self.graph.__contains__(self.startNode)  #ueberprueft, ob Start-Knoten in Graphen ist.
         check2 = self.graph.__contains__(self.endNode)    #ueberprueft, ob End-Knoten in Graphen ist.
@@ -80,30 +99,48 @@ class Road:
                         break
 
         temp = []
-        if (len(self.map) != 0):
-            for node in self.road[0]:
-                temp.append(node)
-            self.road = temp
-        return self.road
 
+        return self.road
+    
 
     # Weg auf der Karte einfügen
     def fillRoad(self, map):
-        for node in self.road:
-            map[node[0]][node[1]] = 15
+        for road in self.road:
+            for node in road:
+                map[node[0]][node[1]] = 15
         return map
+
+
 
     def showMap(self):
         for row in self.map:
             print (row)
-"""
+        print(self.costList)
+
 if __name__ == '__main__':
+
     test = Dungeon(20,20,50)
     #test.multiRoom(2,5,5,1)
-    test.roomInitializing(2,2,3,4,1)
-    test.roomInitializing(15,8,3,2,4)
-    test2 = Road(test.returnArray(),(4,1),(14,5))
+    test.roomInitializing(1,3,4,5,3,2)
+    test.roomInitializing(9,14,6,4,1,2)
+    test.roomInitializing(12,9,6,2,2,2)
+    #test.fillBorder()
 
-    print(test2.fillRoad(test.returnArray()))
 
-"""
+    test2 = Road(test.returnArray())
+    test2.pfad_breitensuche((5,4),(11, 9))
+    #test2.pfad_breitensuche((18,9),(11, 18))
+    test2.pfad_breitensuche((18,9),(8, 14))
+    test2.fillRoad(test.returnArray())
+    print(test.returnArray())
+
+    #print(test2.costList)
+    #print(test2.graph)
+    #print(test2.pfad_breitensuche())
+    #test2.fillRoad(test.returnArray())
+
+    arr = test.returnArray()
+    plt.imshow(arr,interpolation='nearest',cmap=plt.cm.gray)
+    plt.show()
+    #print(test2.fillRoad(test.returnArray()))
+
