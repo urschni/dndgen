6542be668424705019026119f7728aaac48c9c13
Hoggel0815 <39238459+Hoggel0815@users.noreply.github.com>
2018-06-18 12:43:05 +0200

    grafische erweiterung der ausgabe

--- cgi-bin/controller.py
+++ cgi-bin/controller.py
@@ -160,7 +160,20 @@ include_debug = True
 #Print the HTML page for the client
 print("Content-Type: text/html; charset=utf-8\n")
 print("<html>")
+print("<meta charset=\"utf-8\">")
+print("<head> <title>Dungeon Generator</title> ")
+print("<link rel=\"shortcut icon\" href=\"http://localhost:8000/graphics/icon.ico\" type=\"image/vnd.microsoft.icon\" />")
+print("<style>")
+print("body{background-image: url(http://localhost:8000/graphics/bg.png);}")
+print(".content {max-width: 500px;margin: auto;padding: 10px;background-color: rgba(255,255,255,0.85);line-height: 2;font-size: 20px;}")
+print("input{font-size: 18px;}")
+print("select{font-size: 18px;}")
+print(".center {display: block;margin-left: auto;margin-right: auto;}")
+print("</style>")
+print("</head>")
 print("<body>")
+print("<img class=\"center\" src=\"http://localhost:8000/graphics/DungeonGen.png\" alt=\"Dungeon Logo\" style=\"width:500px\">")
+print("<div class=\"content\">")
 if dungeon_name is not None:
     print("<h1>" + dungeon_name + "</h1>")
 else:
@@ -170,5 +183,6 @@ if include_debug:
     print(all_attributes)
 print("<br><img src=\"/my.png\" alt=\"Dungeon Map\"><br>")
 print(encounter)
+print("</div>")
 print("</body>")
 print("</html>")
