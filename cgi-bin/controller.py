1d93b51eae066605c01a3097b70615844caffaaf
Hoggel0815 <39238459+Hoggel0815@users.noreply.github.com>
2018-06-18 19:06:11 +0200

    Update

--- cgi-bin/controller.py
+++ cgi-bin/controller.py
@@ -2,7 +2,6 @@ import cgi, cgitb
 import time
 from DnD import *
 from loot_encounters_gen import *
-import json
 from random import randint
 from PIL import Image
 import numpy as np
@@ -12,68 +11,6 @@ cgitb.enable()
 
 #Timestamp to create unique filenames
 current_milli_time = lambda: int(round(time.time() * 1000.000))
-
-
-'''
-dungeon_size = (30,30)
-site_offset = 1
-inner_dungeon_size = (dungeon_size[0] - 2*site_offset, dungeon_size[1] - 2*site_offset)
-
-w, h = dungeon_size[0], dungeon_size[1]
-data = np.zeros((h, w, 3), dtype=np.uint8)
-
-#Lay out inner quadratic rooms
-number_of_tries = 10
-max_min_roomsize = (2, 5)
-for a in range(number_of_tries):
-    actual_roomsize = randint(max_min_roomsize[0], max_min_roomsize[1])
-    print(actual_roomsize)
-    actual_position = (randint(site_offset, inner_dungeon_size[0] - actual_roomsize), randint(site_offset, inner_dungeon_size[1] -  actual_roomsize))
-    print(actual_position)
-    is_free = True
-    for x in range(actual_position[0] - 1, actual_position[0] + actual_roomsize + 1):
-        for y in range(actual_position[1] - 1, actual_position[1] + actual_roomsize + 1):
-            """if x > inner_dungeon_size[0] or y > inner_dungeon_size[1] or x < :
-                is_working = False
-                break
-            """
-            if np.array_equal(data[x][y], [255,255,255]):
-                is_free = False
-                break
-    if is_free:
-        data[actual_position[0]:actual_position[0] + actual_roomsize, actual_position[1]:actual_position[1] + actual_roomsize] = [255,255,255]
-
-#Markings for axis
-#Pink
-data[3:20,3] = [255,0,255]
-#Blue
-data[4,4:20] = [0,255,255]
-
-#Generate entrances
-number_of_entrances = 2
-entrances = []
-#0 - north, 1 - east, 2 - south, 3 - west
-while(len(entrances) < number_of_entrances):
-    direction = randint(0, 3)
-    if direction == 0 or direction == 2:
-        pos = randint(0, dungeon_size[0] - 1)
-        if direction == 0:
-            entrances.append((pos, 0))
-        else:
-            entrances.append((pos, dungeon_size[1] - 1))
-    else:
-        pos = randint(0, dungeon_size[1] - 1)
-        if direction == 3:
-            entrances.append((0, pos))
-        else: 
-            entrances.append((dungeon_size[1] - 1, pos))
-print("Entrance positions:")
-for entrance_pos in entrances:
-    print(entrance_pos)
-    data[entrance_pos[0],entrance_pos[1]] = [255,255,255]
-
-'''
-
 
 #Get the values from the form
 form = cgi.FieldStorage()
@@ -129,7 +66,7 @@ loot_allow = yes_no_to_bool[loot_allow]
 
 #Create Dungeon
 dungeon = Dungeon(dungeon_size[0], dungeon_size[1], 50)
-dungeon.multiRoom(5, max_room_size[0], max_room_size[1], 1)
+#dungeon.multiRoom(5, max_room_size[0], max_room_size[1], 1)
 map = dungeon.returnArray()
 
 #Generate encounter
