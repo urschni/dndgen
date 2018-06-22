19452291e86186353ceddd236d47a913b439b73e
SirKero <sirkero@gmx.de>
2018-06-22 17:47:48 +0200

    Added processing in controller

--- cgi-bin/controller.py
+++ cgi-bin/controller.py
@@ -15,7 +15,10 @@ current_milli_time = lambda: int(round(time.time() * 1000.000))
 #Get the values from the form
 form = cgi.FieldStorage()
 dungeon_name = form.getvalue('dungeon_name')
+dungeon_height = form.getvalue('dungeon_height')
+dungeon_length = form.getvalue('dungeon_length')
 dungeon_size = form.getvalue('dungeon_size')
+room_density = form.getvalue('room_density')
 dungeon_lvl = form.getvalue('dungeon_lvl')
 party_size = form.getvalue('party_size')
 monster_allow = form.getvalue('monster_allow')
@@ -24,6 +27,7 @@ trap_allow = form.getvalue('trap_allow')
 trap_freq = form.getvalue('trap_freq')
 deadend_allow = form.getvalue('deadend_allow')
 loot_allow = form.getvalue('loot_allow')
+img_res = form.getvalue('img_res')
 
 #Debug message with all attribtes from the HTML-form
 all_attributes = ""
@@ -33,15 +37,28 @@ for x in form:
 yes_no_to_bool = {'yes': True, 'no': False, None: None}
 #Process HTML form data
 #Process dungeon_size
-if 'small' == dungeon_size:
-    dungeon_size = (10, 10)
-    max_room_size = (3, 3)
-elif 'medium' == dungeon_size:
-    dungeon_size = (20, 20)
-    max_room_size = (5, 5)
+if dungeon_height and dungeon_length not None:
+	height = int(dungeon_height)
+	length = int(dungeon_length)
+	dungeon_size = (length,height)
+	max_room_size = (int(length/4),int(height/4))
 else:
-    dungeon_size = (30, 30)
-    max_room_size = (7, 7)
+	if 'small' == dungeon_size:
+		dungeon_size = (10, 10)
+		max_room_size = (3, 3)
+	elif 'medium' == dungeon_size:
+		dungeon_size = (20, 20)
+		max_room_size = (5, 5)
+	else:
+		dungeon_size = (30, 30)
+		max_room_size = (7, 7)
+#Process room_density
+if room_density == 'low':
+	room_density = 0.5
+elif room_density == 'medium':
+	room_density = 1
+else:
+	room_density = 1.5
 #Process dungeon_lvl
 dungeon_lvl = int(dungeon_lvl)
 #Process party_size
@@ -63,6 +80,19 @@ else:
 deadend_allow = yes_no_to_bool[deadend_allow]
 #Process loot_allow
 loot_allow = yes_no_to_bool[loot_allow]
+#Process img_res
+if img_res == 'low':
+	img_len = 250
+elif img_res == 'medium':
+	img_len = 500
+elif img_res == 'high':
+	img_len = 1000
+elif img_res == 'very_high':
+	img_len = 2000
+else:
+	img_len = 4000
+img_height = int(img_len/dungeon_size[0])*dungeon_size[1]
+img_res = (img_len,img_height)
 
 #Create Dungeon
 dungeon = Dungeon(dungeon_size[0], dungeon_size[1], 50)
