abd50ee34c6e1569f939836b11530ca8e7d54d3d
SirKero <sirkero@gmx.de>
2018-07-03 23:13:00 +0200

    added thread for the cleaner

--- mywebserver.py
+++ mywebserver.py
@@ -1,10 +1,21 @@
 from http.server import HTTPServer, CGIHTTPRequestHandler
-import cgitb;
+from threading import Thread
+import cgitb, sys
+sys.path.insert(0,'./cgi-bin')
+import img_cleaner 
+
 cgitb.enable()  ## This line enables CGI error reporting
  
 server = HTTPServer
 handler = CGIHTTPRequestHandler
 server_address = ("", 8000)
+cleaner_thread = Thread(target = img_cleaner.thread)
+print("server started")
+try:
+	cleaner_thread.start()
+except (KeyboardInterrupt, SystemExit):
+	cleanup_stop_thread()
+	sys.exit
  
 httpd = server(server_address, handler)
 httpd.serve_forever()
